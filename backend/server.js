a461fb6ed56c08c6f0e01b69b32dad037229d8f1
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-05-19 14:59:32 +0530

    fixed delete issue

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const colors=require('colors');
 const connectDb = require('./config/db');
 
 require('dotenv').config()
-const PORT=process.env.PORT||3000;
+const PORT=process.env.PORT||8000;
 connectDb();
 const app=express();
 
